221a717b07a6b3510a9c46700044439e36b2db95
Naveen Kewalramani <naveen.kewalramani2903@gmail.com>
2024-02-27 16:15:56 +0530

    Update main.go

--- main/main.go
+++ main/main.go
@@ -7,8 +7,13 @@ func main() {
 	Naveen := 1
 	fmt.Println(Naveen)
 	Adnan := 2
+	Adnan1 := 32
 }
 
 func declaredButNotUsed(str string) {
+	fmt.Println(str)
+}
+
+func DeclaredButNotUsed1(str string) {
 	fmt.Println(str)
 }
